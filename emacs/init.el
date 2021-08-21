616123f0a7135a98d9461e41dd42ffe861230249
Greg Peddle <gpeddle@gmail.com>
2021-08-21 16:40:11 -0400

    add emacs/docker

---
+++ emacs/init.el
@@ -0,0 +1,82 @@
+;;prefer a minimalist ui
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+;(menu-bar-mode -1)
+(setq visible-bell t)
+
+
+;; NOTE: install fonts-firacode on system first
+;; `sudo apt install fonts-firacode`
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 160)
+
+
+(load-theme 'tango-dark)
+
+;; initialize package sources
+(require 'package)
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("melpa-stable" . "https://stable.melpa.org/packages/")
+        ("org" . "https://orgmode.org/elpa/")
+        ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+
+;; initialize use-package  if needed
+;; https://github.com/jwiegley/use-package
+(unless (package-installed-p 'use-package) 
+	(package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package command-log-mode)
+
+;; NOTE: install ivy/counsel first
+;; `M-x package-install RET counsel RET`
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . iy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-f" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+;; clm/toggle-command-log
+;;(clm/toggle-command-log)
+;; global-command-log-mode
+;;(global-command-log-mode)
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(counsel ivy command-log-mode use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
